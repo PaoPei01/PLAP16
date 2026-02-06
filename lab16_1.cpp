db2f78ca7cb7d1aa544e2ba6e99a10f4dca50125
PaoPei01 <paopei2706@gmail.com>
2026-02-06 22:37:56 +0700

    lab16_1.cpp completed

--- lab16_1.cpp
+++ lab16_1.cpp
@@ -4,13 +4,40 @@
 #include<iomanip>
 using namespace std;
 
-void showData(double *,int,int);
+void showData(double *dPtr, int N, int M) {
+    cout << fixed << setprecision(2);
+    for(int i = 0; i < N*M; i++) {
+        cout << dPtr[i];
+        if((i + 1) % M == 0) cout << endl;
+        else cout << " ";
+    }
+}
 
-void randData(double *,int,int);
+void randData(double *dPtr, int N, int M) {
+    for(int i = 0; i < N*M; i++) {
+        dPtr[i] = (rand() % 101) / 100.0;
+    }
+}
 
-void findRowSum(const double *,double *,int,int);
+void findRowSum(const double *dPtr, double *sum1, int N, int M) {
+    for(int i = 0; i < N; i++) {
+        double rowSum = 0;
+        for(int j = 0; j < M; j++) {
+            rowSum += dPtr[i * M + j];
+        }
+        sum1[i] = rowSum;
+    }
+}
 
-void findColSum(const double *,double *,int,int);
+void findColSum(const double *dPtr, double *sum2, int N, int M) {
+    for(int j = 0; j < M; j++) {
+        double colSum = 0;
+        for(int i = 0; i < N; i++) {
+            colSum += dPtr[i * M + j];
+        }
+        sum2[j] = colSum;
+    }
+}
 
 int main(){
 	srand(time(0));
